dc415c3316f14f756423288c666cfd9cdaa7fa92
dorothyDing <3267763309@qq.com>
2023-03-23 14:06:39 +0800

    feat: update
    Signed-off-by: dorothyDing <3267763309@qq.com>

--- a.js
+++ a.js
@@ -2,7 +2,7 @@ function sun(a, b) {
   return a + b
 }
 
-onst promise = new Promise((resolve, reject) => {
+const promise = new Promise((resolve, reject) => {
   // 异步操作
   if ('异步操作成功') {
     resolve(value);
