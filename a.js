99cd8f2228f4c644b1449470893a113a1e4bd29e
dorothyDing <3267763309@qq.com>
2023-03-23 13:42:33 +0800

    feat: add promise
    Signed-off-by: dorothyDing <3267763309@qq.com>

--- a.js
+++ a.js
@@ -2,4 +2,13 @@ function sun(a, b) {
   return a + b
 }
 
+onst promise = new Promise((resolve, reject) => {
+  // 异步操作
+  if ('异步操作成功') {
+    resolve(value);
+  } else {
+    reject(error);
+  }
+});
+
 let count = sun(a + b)
